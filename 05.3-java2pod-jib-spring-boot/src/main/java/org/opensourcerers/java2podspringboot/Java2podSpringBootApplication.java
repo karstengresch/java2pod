6cc4b8e94dbde11e5673e63c78f3df7f63687339
Karsten Gresch <git-2015@by-doing.org>
2023-05-10 15:22:44 +0200

    added Spring Boot

---
+++ 05.3-java2pod-jib-spring-boot/src/main/java/org/opensourcerers/java2podspringboot/Java2podSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.opensourcerers.java2podspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2podSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java2podSpringBootApplication.class, args);
+	}
+
+}
---
+++ 05.3-java2pod-jib-spring-boot/src/test/java/org/opensourcerers/java2podspringboot/Java2podSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opensourcerers.java2podspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2podSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
