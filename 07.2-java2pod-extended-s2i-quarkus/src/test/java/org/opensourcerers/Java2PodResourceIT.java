7cec0ecdbbd50c58d48c3c280c00d517c97e8965
Karsten Gresch <git-2015@by-doing.org>
2023-05-12 20:31:32 +0200

    refactor, taking out s2i quarkus

--- 07.2-java2pod-extended-s2i-quarkus/src/test/java/org/opensourcerers/Java2PodResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.opensourcerers;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class Java2PodResourceIT extends Java2PodResourceTest {
-    // Execute the same tests but in packaged mode.
-}
