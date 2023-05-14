dc2a5f57d71354e7a82c39de49ba742659655227
Karsten Gresch <git-2015@by-doing.org>
2023-05-14 12:15:49 +0200

    buildpacks - push

---
+++ 09.1-java2pod-extended-buildpacks-basic/src/test/java/org/opensourcerers/Java2PodResourceIT.java
@@ -0,0 +1,8 @@
+package org.opensourcerers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class Java2PodResourceIT extends Java2PodResourceTest {
+    // Execute the same tests but in packaged mode.
+}
