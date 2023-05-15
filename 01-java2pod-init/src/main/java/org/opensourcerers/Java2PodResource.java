38e260581849ca73db2ee379bf07eb52a6879c9d
Karsten Gresch <git-2015@by-doing.org>
2023-05-15 15:23:06 +0200

    ongoing

--- 01-java2pod-init/src/main/java/org/opensourcerers/Java2PodResource.java
+++ 01-java2pod-init/src/main/java/org/opensourcerers/Java2PodResource.java
@@ -1,8 +1,7 @@
 package org.opensourcerers;
 
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/java2pod")
